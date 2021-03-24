00394c3df584b5e7c2679452de568c2b4c0b79c2
Sirazad <lajosimola2@gmail.com>
2021-03-24 15:19:43 +0100

    passed 3 from 9, not clean yet

--- src/main/java/com/codecool/farm/Farm.java
+++ src/main/java/com/codecool/farm/Farm.java
@@ -1,5 +1,38 @@
 package com.codecool.farm;
 
+import com.codecool.farm.animal.Animal;
+
+import java.util.ArrayList;
+import java.util.List;
+
 class Farm {
+    private List<Animal> animals = new ArrayList<>();
+
+    Farm(List animals) {
+        this.animals = animals;
+    }
+
+    void feedAnimals() {
+//    feladata: A farmon lévő összes állat megetetése.
+    }
+
+
+    void butcher(Butcher butcher) {
+    // összes levágható állat eltávolítása a farmon lévő állatokat tartalmazó listából
+    }
+
+    boolean isEmpty() {
+      return (animals.isEmpty())? true : false;
+    }
+    List<Animal> getAnimals() {
+        return animals;
+    }
+    List<String> getStatus() {
+     //állatok státuszait tartalmazó listát ad vissza,
+        // a farmon lévő összes állatra vonatkozóan.
+        //"There is a <állat mérete> sized <állat típusa (pig/cattle)> in the farm."
+        return null;
+    }
+
 
 }
